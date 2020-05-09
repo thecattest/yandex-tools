8a56d9760800ea0fa0f6251672c09ac4937bfcea
vodopyanov ilya <vodopyanov999@gmail.com>
2020-05-09 12:49:48 +0300

    count blocks too

--- count_tasks.py
+++ count_tasks.py
@@ -7,26 +7,29 @@ course_id, group_id = get_course(s)
 
 lesson_ids = get_lesson_ids(s, course_id, group_id)
 tasks_number = defaultdict(int)
+task_groups_number = defaultdict(int)
 scores = defaultdict(int)
 
 
 for lesson_id in lesson_ids:
     lesson_info = get_all_tasks(s, lesson_id, course_id)
     for task_group in lesson_info:
+        task_groups_number[task_group['type']] += 1
         for task in task_group['tasks']:
             type = task['tag']['type']
             score = task['scoreMax']
             tasks_number[type] += 1
             scores[type] += score
 
-tasks_number['control-work'] += tasks_number['additional-3']
-del tasks_number['additional-3']
-scores['control-work'] += scores['additional-3']
-del scores['additional-3']
 
 print("Количество задач\n")
 print(*list(f"{titles[key]}:\t{item}" for key, item in tasks_number.items()), sep='\n')
 print("=========\n")
+
+print("Количество блоков\n")
+print(*list(f"{titles[key]}:\t{item}" for key, item in task_groups_number.items()), sep='\n')
+print("=========\n")
+
 print("Первичные баллы\n")
 print(*list(f"{titles[key]}:\t{item}" for key, item in scores.items()), sep='\n')
 
